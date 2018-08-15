c6eb6893ec7ae30cccf8fe1b2f26ac18813f6c6a
童江松 <595696730@qq.com>
2018-08-15 17:50:27 +0800

    add-demo

---
+++ ffplay_demo/ffplay_demo/ffplay.h
@@ -0,0 +1,34 @@
+#pragma once
+
+extern "C"
+{
+
+#include "libavutil/avstring.h"
+#include "libavutil/eval.h"
+#include "libavutil/mathematics.h"
+#include "libavutil/pixdesc.h"
+#include "libavutil/imgutils.h"
+#include "libavutil/dict.h"
+#include "libavutil/parseutils.h"
+#include "libavutil/samplefmt.h"
+#include "libavutil/avassert.h"
+#include "libavutil/time.h"
+#include "libavformat/avformat.h"
+#include "libavdevice/avdevice.h"
+#include "libswscale/swscale.h"
+#include "libavutil/opt.h"
+#include "libavcodec/avfft.h"
+#include "libswresample/swresample.h"
+	//#if CONFIG_AVFILTER
+# include "libavfilter/avfilter.h"
+# include "libavfilter/buffersink.h"
+# include "libavfilter/buffersrc.h"
+
+	//#endif
+#include <SDL.h>
+#include <SDL_thread.h>
+
+
+
+#include <assert.h>
+};
